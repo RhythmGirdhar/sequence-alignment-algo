054eea9d969c09813f0dca3170b97a5d21016621
Rhythm Girdhar <ridhigirdhar3@gmail.com>
2022-05-06 13:02:16 -0700

    Starter Code with latest updates

--- efficient_3.py
+++ efficient_3.py
@@ -1,4 +1,7 @@
 import sys
+import time
+import os
+import psutil
 
 INPUT_FILE = sys.argv[1]
 OUTPUT_FILE = sys.argv[2]
@@ -22,47 +25,8 @@ def generate_matching(s1: str, s2: str):
     """
     m = len(s1)
     n = len(s2)
-    opt = memoize(x, y)
-    print(opt[m][n])
-    cur_i = m
-    cur_j = n
-    matching_s1 = ""
-    matching_s2 = ""
-    while cur_i > 0 and cur_j > 0:
-        cost_s1_s2 = opt[cur_i - 1][cur_j - 1]
-        cost_s1_delta = opt[cur_i - 1][cur_j]
-        cost_s2_delta = opt[cur_i][cur_j - 1]
-        prev = min(
-            cost_s1_s2,
-            cost_s1_delta,
-            cost_s2_delta
-        )
-        if prev == cost_s1_delta:
-            # If (cur_i)th character of s1 is not matched
-            matching_s1 += s1[cur_i - 1]
-            matching_s2 += "_"
-            cur_i -= 1
-        elif prev == cost_s2_delta:
-            # If (cur_j)th character of s2 is not matched
-            matching_s1 += "_"
-            matching_s2 += s2[cur_j - 1]
-            cur_j -= 1
-        else:
-            # Both (cur_i)th and (cur_j)th characters are present in the matching
-            matching_s1 += s1[cur_i - 1]
-            matching_s2 += s2[cur_j - 1]
-            cur_i -= 1
-            cur_j -= 1
-
-    while cur_i > 0:
-        matching_s1 += s1[cur_i - 1]
-        matching_s2 += "_"
-        cur_i -= 1
-    while cur_j > 0:
-        matching_s2 += s2[cur_j - 1]
-        matching_s1 += "_"
-        cur_j -= 1
-    return matching_s1[::-1], matching_s2[::-1]
+    
+    return s1,s2
 
 def generate_sequences(input_file: str) -> (str, str):
     """
@@ -77,7 +41,16 @@ def generate_sequences(input_file: str) -> (str, str):
     s2 = " "
     return s1, s2
 
+start_time = time.time()
 x, y = generate_sequences(INPUT_FILE)
-matching_x, matching_y = generate_matching(x, y)
-print(matching_x)
-print(matching_y)
+matching_x, matching_y, min_cost = generate_matching(x, y)
+end_time = time.time()
+time_taken = 1000*(end_time-start_time)
+process = psutil.Process(os.getpid())
+memory_used = int(process.memory_info().rss / 1024)
+with open(OUTPUT_FILE, "w") as f:
+    f.write(str(min_cost)+"\n")
+    f.write(matching_x+"\n")
+    f.write(matching_y+"\n")
+    f.write(str(time_taken)+"\n")
+    f.write(str(memory_used)+"\n")
